ade542b9f6e972156d13feebbf3d1a363bd6ff9d
thientranreal <116498615+thientranreal@users.noreply.github.com>
2023-03-18 20:34:40 +0700

    QL ban sach

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import GUI.HoaDon_GUI;
+import GUI.ThongKe_GUI;
+
+public class Main {
+    public static void main(String[] args) {
+//        new ThongKe_GUI();
+        new HoaDon_GUI();
+    }
+}
